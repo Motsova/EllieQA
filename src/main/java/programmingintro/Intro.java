b7244bb9906eb72eec3a4896affd1b530e64a11f
Motsova <motsova@gmail.com>
2023-01-02 17:10:13 +0200

    main method and console interactions added

---
+++ src/main/java/programmingintro/Intro.java
@@ -0,0 +1,27 @@
+package programmingintro;
+
+import java.util.Scanner;
+
+public class Intro {
+public static void main(String[] args) {
+
+    System.out.println("Please enter your name:");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println("Hi, "+ name);
+
+    System.out.println("Please enter your age:");
+    int age = scanner.nextInt();
+
+    System.out.println("Hi, "+ name + " your age is "  + age);
+
+    if (age >= 18) {
+        System.out.println(name + " is able to drink ");
+    }else {
+        System.out.println(name + " is NOT able to drink ");
+    }
+}
+
+
+
+}
